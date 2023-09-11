38a8c943bfd9cfecc619ed6297f9074c6e824991
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-11 14:50:07 -0300

    feat: ensure AccountRepository create a account on success

--- src/infra/database/postgres/repositories/account.ts
+++ src/infra/database/postgres/repositories/account.ts
@@ -1,10 +1,14 @@
-import { type CheckAccountByEmail } from '@/domain/contracts/database/repositories/account'
+import { type CheckAccountByEmail, type AddAccountRepository } from '@/domain/contracts/database/repositories/account'
 import { prisma } from '@/infra/database/postgres/helpers'
 
-export class AccountRepository implements CheckAccountByEmail {
+export class AccountRepository implements CheckAccountByEmail, AddAccountRepository {
   async checkByEmail ({ email }: CheckAccountByEmail.Input): Promise<CheckAccountByEmail.Output> {
     const isValid = await prisma.user.findFirst({ where: { email } })
 
     return isValid != null
+  }
+
+  async create ({ email, name, password }: AddAccountRepository.Input): Promise<AddAccountRepository.Output> {
+    await prisma.user.create({ data: { email, name, password } })
   }
 }
