4c3574d97333c6494c2a1cb5467830ee3c72b4cb
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-11 12:43:56 -0300

    test: ensure AddAccountUseCase call HashGenerator with correct plaintext

--- tests/domain/use-cases/account/add-account.spec.ts
+++ tests/domain/use-cases/account/add-account.spec.ts
@@ -1,6 +1,7 @@
 import { accountParams } from '@/tests/mocks/'
 import { type CheckAccountByEmail } from '@/domain/contracts/database/repositories/account'
 import { type AddAccount, addAccountUseCase } from '@/domain/use-cases/account'
+import { type HashGenerator } from '@/domain/contracts/gateways'
 import { FieldInUseError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
@@ -11,13 +12,14 @@ describe('AddAccount', () => {
   const { name, email, password } = accountParams
 
   const accountRepository = mock<CheckAccountByEmail>()
+  const hash = mock<HashGenerator>()
 
   beforeAll(() => {
     accountRepository.checkByEmail.mockResolvedValueOnce(false)
   })
 
   beforeEach(() => {
-    sut = addAccountUseCase(accountRepository)
+    sut = addAccountUseCase(accountRepository, hash)
   })
 
   it('should call CheckAccountByEmail with correct email', async () => {
@@ -33,5 +35,12 @@ describe('AddAccount', () => {
     const promise = sut({ name, email, password })
 
     await expect(promise).rejects.toThrow(new FieldInUseError('email'))
+  })
+
+  it('should call HashGenerator with correct plaintext', async () => {
+    await sut({ name, email, password })
+
+    expect(hash.generate).toHaveBeenCalledWith({ plaintext: password })
+    expect(hash.generate).toHaveBeenCalledTimes(1)
   })
 })
