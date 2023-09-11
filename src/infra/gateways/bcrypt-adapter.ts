3d3efd59dc53adbcbc14d1af1a4b42e051bcbb9a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-11 13:39:07 -0300

    feat: ensure BcryptAdapter return a digest on success

--- src/infra/gateways/bcrypt-adapter.ts
+++ src/infra/gateways/bcrypt-adapter.ts
@@ -2,8 +2,8 @@ import { type HashGenerator } from '@/domain/contracts/gateways'
 
 import * as bcrypt from 'bcrypt'
 
-export class BcryptAdapter {
-  async generate ({ plaintext }: HashGenerator.Input): Promise<void> {
-    await bcrypt.hash(plaintext, 12)
+export class BcryptAdapter implements HashGenerator {
+  async generate ({ plaintext }: HashGenerator.Input): Promise<HashGenerator.Output> {
+    return await bcrypt.hash(plaintext, 12)
   }
 }
