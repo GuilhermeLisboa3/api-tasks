2aede5494cb85b5c89c23c9aa95d67fd36c6604e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-12 14:35:19 -0300

    feat: ensure Controller return serverError if perform throw

---
+++ src/application/controllers/controller.ts
@@ -0,0 +1,12 @@
+import { serverError, type HttpResponse } from '@/application/helpers'
+
+export abstract class Controller {
+  abstract perform (httpRequest?: any): Promise<HttpResponse>
+  async handle (httpRequest?: any): Promise<HttpResponse> {
+    try {
+      return await this.perform(httpRequest)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
