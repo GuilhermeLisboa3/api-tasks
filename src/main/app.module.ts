3556e6ed8b8ffe118f398bf20302ee4066e4f4a0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-14 15:42:12 -0300

    feat: create ValidationPipe

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
 import { RoutesModule } from './routes/routes.module'
+import { APP_PIPE } from '@nestjs/core'
+import { ValidationPipe } from './validations/intercept-validation'
 @Module({
   imports: [
     RoutesModule
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe
+    }
   ]
 })
 export class AppModule {}
