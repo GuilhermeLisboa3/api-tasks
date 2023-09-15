ca5dd3c8b495da7017fc9b89644e8ab7e4751b0b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-15 15:10:53 -0300

    test: ensure route post /register return 400 if email already exists

--- tests/main/routes/account.test.ts
+++ tests/main/routes/account.test.ts
@@ -3,6 +3,8 @@ import { Test } from '@nestjs/testing'
 import { type INestApplication } from '@nestjs/common'
 import { accountParams } from '@/tests/mocks'
 import { RoutesModule } from '@/main/routes/routes.module'
+import { FieldInUseError } from '@/domain/errors'
+import { prisma } from '@/infra/database/postgres/helpers'
 
 describe('Account Route', () => {
   let app: INestApplication
@@ -28,6 +30,16 @@ describe('Account Route', () => {
         .post('/register')
         .send({ name, email, password })
         .expect(201)
+    })
+
+    it('should return 400 if email already exists', async () => {
+      await prisma.user.create({ data: { name, email, password } })
+      const { status, body: { error } } = await request(app.getHttpServer())
+        .post('/register')
+        .send({ name, email, password })
+
+      expect(status).toBe(400)
+      expect(error).toEqual(new FieldInUseError('email').message)
     })
   })
 })
