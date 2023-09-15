d3cf8e3de19fbcdb3647f6637656e27faef60b68
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-15 17:26:02 -0300

    feat: ensure Authentication throw AuthenticationError if LoadAccountByEmailRepository return null

--- src/domain/use-cases/account/authentication.ts
+++ src/domain/use-cases/account/authentication.ts
@@ -1,4 +1,5 @@
 import { type LoadAccountByEmail } from '@/domain/contracts/database/repositories/account'
+import { AuthenticationError } from '@/domain/errors'
 
 type Setup = (accountRepository: LoadAccountByEmail) => Authentication
 type Input = { email: string, password: string }
@@ -6,5 +7,6 @@ type Output = void
 export type Authentication = (input: Input) => Promise<Output>
 
 export const authenticationUseCase: Setup = (accountRepository) => async ({ email, password }) => {
-  await accountRepository.loadByEmail({ email })
+  const account = await accountRepository.loadByEmail({ email })
+  if (!account) throw new AuthenticationError()
 }
