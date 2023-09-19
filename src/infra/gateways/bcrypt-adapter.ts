557b6187e28b9c203662c976614c213a4669a292
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-19 14:03:44 -0300

    feat: ensure BcryptAdapter return true when compare succeeds

--- src/infra/gateways/bcrypt-adapter.ts
+++ src/infra/gateways/bcrypt-adapter.ts
@@ -2,12 +2,12 @@ import { type HashGenerator, type HashComparer } from '@/domain/contracts/gatewa
 
 import * as bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements HashGenerator {
+export class BcryptAdapter implements HashGenerator, HashComparer {
   async generate ({ plaintext }: HashGenerator.Input): Promise<HashGenerator.Output> {
     return await bcrypt.hash(plaintext, 12)
   }
 
-  async compare ({ digest, plaintext }: HashComparer.Input): Promise<void> {
-    await bcrypt.compare(plaintext, digest)
+  async compare ({ digest, plaintext }: HashComparer.Input): Promise<HashComparer.Output> {
+    return await bcrypt.compare(plaintext, digest)
   }
 }
