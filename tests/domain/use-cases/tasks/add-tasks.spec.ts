783fbc8cdd74ed22ab5e7455cb992d79edbbe17d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-28 14:47:50 -0300

    test: ensure addTasksUseCase throw NotFoundError if LoadAccountById return undefined

--- tests/domain/use-cases/tasks/add-tasks.spec.ts
+++ tests/domain/use-cases/tasks/add-tasks.spec.ts
@@ -1,16 +1,21 @@
 import { accountParams, tasksParams } from '@/tests/mocks/'
 import { type LoadAccountById } from '@/domain/contracts/database/repositories/account'
 import { type AddTasks, addTasksUseCase } from '@/domain/use-cases/tasks'
+import { NotFoundError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
 describe('AddTasks', () => {
   let sut: AddTasks
 
-  const { id: accountId } = accountParams
+  const { id: accountId, email, name, password } = accountParams
   const { title, description } = tasksParams
 
   const accountRepository = mock<LoadAccountById>()
+
+  beforeAll(() => {
+    accountRepository.loadById.mockResolvedValue({ id: accountId, name, password, email })
+  })
 
   beforeEach(() => {
     sut = addTasksUseCase(accountRepository)
@@ -21,5 +26,13 @@ describe('AddTasks', () => {
 
     expect(accountRepository.loadById).toHaveBeenCalledWith({ id: accountId })
     expect(accountRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw NotFoundError if LoadAccountById return undefined', async () => {
+    accountRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut({ accountId, title, description })
+
+    await expect(promise).rejects.toThrow(new NotFoundError('accountId'))
   })
 })
