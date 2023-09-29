06f1674e361bcf2133c76fb70296353df24fe9eb
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-29 16:12:38 -0300

    test: ensure authorizeUseCase call LoadAccountById with correct value

--- tests/domain/use-cases/account/authorize.spec.ts
+++ tests/domain/use-cases/account/authorize.spec.ts
@@ -1,6 +1,7 @@
 import { accountParams } from '@/tests/mocks'
 import { type Authorize, authorizeUseCase } from '@/domain/use-cases/account'
 import { type TokenValidator } from '@/domain/contracts/gateways'
+import { type LoadAccountById } from '@/domain/contracts/database/repositories/account'
 import { AuthenticationError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
@@ -9,10 +10,15 @@ describe('AuthorizeUseCase', () => {
   let sut: Authorize
 
   const token = mock<TokenValidator>()
-  const { accessToken, error } = accountParams
+  const accountRepository = mock<LoadAccountById>()
+  const { accessToken, error, id } = accountParams
+
+  beforeAll(() => {
+    token.validate.mockResolvedValue(id)
+  })
 
   beforeEach(() => {
-    sut = authorizeUseCase(token)
+    sut = authorizeUseCase(token, accountRepository)
   })
 
   it('should call TokenValidator with correct value', async () => {
@@ -27,5 +33,12 @@ describe('AuthorizeUseCase', () => {
     const promise = sut({ accessToken })
 
     await expect(promise).rejects.toThrow(new AuthenticationError())
+  })
+
+  it('should call LoadAccountById with correct value', async () => {
+    await sut({ accessToken })
+
+    expect(accountRepository.loadById).toHaveBeenCalledWith({ id })
+    expect(accountRepository.loadById).toHaveBeenCalledTimes(1)
   })
 })
