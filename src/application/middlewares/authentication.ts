f8396dcea6c9f06024090d0fb3cadfdb808f8201
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-29 17:27:46 -0300

    feat: ensure AuthenticationMiddleware return unauthorized if authorize return AuthenticationError

--- src/application/middlewares/authentication.ts
+++ src/application/middlewares/authentication.ts
@@ -1,6 +1,7 @@
 import { type HttpResponse, unauthorized } from '@/application/helpers'
 import { type Middleware } from '@/application/middlewares'
 import { type Authorize } from '@/domain/use-cases/account'
+import { AuthenticationError } from '@/domain/errors'
 
 type HttpRequest = { authorization: string }
 
@@ -8,8 +9,12 @@ export class AuthenticationMiddleware implements Middleware {
   constructor (private readonly authorize: Authorize) {}
 
   async handle ({ authorization }: HttpRequest): Promise<HttpResponse> {
-    if (!authorization) return unauthorized()
-    const [, accessToken] = authorization.split(' ')
-    await this.authorize({ accessToken })
+    try {
+      if (!authorization) return unauthorized()
+      const [, accessToken] = authorization.split(' ')
+      await this.authorize({ accessToken })
+    } catch (error) {
+      if (error instanceof AuthenticationError) return unauthorized()
+    }
   }
 }
