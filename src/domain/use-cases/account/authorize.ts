a65c27d87bca0fa1ac228af2df7d819578ef54d7
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-29 16:11:23 -0300

    feat: ensure authorizeUseCase call LoadAccountById with correct value

--- src/domain/use-cases/account/authorize.ts
+++ src/domain/use-cases/account/authorize.ts
@@ -1,13 +1,16 @@
 import { type TokenValidator } from '@/domain/contracts/gateways'
 import { AuthenticationError } from '@/domain/errors'
+import { type LoadAccountById } from '@/domain/contracts/database/repositories/account'
 
-type Setup = (token: TokenValidator) => Authorize
+type Setup = (token: TokenValidator, accountRepository: LoadAccountById) => Authorize
 type Input = { accessToken: string }
 type Output = void
 export type Authorize = (input: Input) => Promise<Output>
 
-export const authorizeUseCase: Setup = (token) => async ({ accessToken }) => {
+export const authorizeUseCase: Setup = (token, accountRepository) => async ({ accessToken }) => {
+  let accountId: string
   try {
-    await token.validate({ token: accessToken })
+    accountId = await token.validate({ token: accessToken })
   } catch (error) { throw new AuthenticationError() }
+  await accountRepository.loadById({ id: accountId })
 }
