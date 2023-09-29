af3e6a09feebe6f127732c18e9faf36ce8c9ddfc
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-29 17:23:46 -0300

    feat: ensure AuthenticationMiddleware call authorize with correct values

--- src/application/middlewares/authentication.ts
+++ src/application/middlewares/authentication.ts
@@ -1,10 +1,15 @@
 import { type HttpResponse, unauthorized } from '@/application/helpers'
 import { type Middleware } from '@/application/middlewares'
+import { type Authorize } from '@/domain/use-cases/account'
 
 type HttpRequest = { authorization: string }
 
 export class AuthenticationMiddleware implements Middleware {
+  constructor (private readonly authorize: Authorize) {}
+
   async handle ({ authorization }: HttpRequest): Promise<HttpResponse> {
     if (!authorization) return unauthorized()
+    const [, accessToken] = authorization.split(' ')
+    await this.authorize({ accessToken })
   }
 }
