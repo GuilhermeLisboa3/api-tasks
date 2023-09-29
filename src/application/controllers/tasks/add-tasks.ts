a8ed711e917a7032321e3c590b4c793c1771d457
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-28 22:31:33 -0300

    feat: ensure AddTasksController extend Controller

--- src/application/controllers/tasks/add-tasks.ts
+++ src/application/controllers/tasks/add-tasks.ts
@@ -1,11 +1,14 @@
 import { type AddTasks } from '@/domain/use-cases/tasks'
+import { Controller } from '../controller'
+import { type HttpResponse } from '@/application/helpers'
 
 type HttpRequest = { title: string, description: string, accountId: string }
 
-export class AddTasksController {
-  constructor (private readonly addTasks: AddTasks) {}
+export class AddTasksController extends Controller {
+  constructor (private readonly addTasks: AddTasks) { super() }
 
-  async perform (input: HttpRequest): Promise<void> {
+  async perform (input: HttpRequest): Promise<HttpResponse> {
     await this.addTasks(input)
+    return { statusCode: 200, data: null }
   }
 }
