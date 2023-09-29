45e7a06e2d84acbfee93a8f9382c24e85f1f8496
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-29 16:35:39 -0300

    test: ensure authorizeUseCase throw PermissionError if LoadAccountById return undefined

--- tests/domain/use-cases/account/authorize.spec.ts
+++ tests/domain/use-cases/account/authorize.spec.ts
@@ -2,7 +2,7 @@ import { accountParams } from '@/tests/mocks'
 import { type Authorize, authorizeUseCase } from '@/domain/use-cases/account'
 import { type TokenValidator } from '@/domain/contracts/gateways'
 import { type LoadAccountById } from '@/domain/contracts/database/repositories/account'
-import { AuthenticationError } from '@/domain/errors'
+import { AuthenticationError, PermissionError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -11,10 +11,11 @@ describe('AuthorizeUseCase', () => {
 
   const token = mock<TokenValidator>()
   const accountRepository = mock<LoadAccountById>()
-  const { accessToken, error, id } = accountParams
+  const { accessToken, error, id, name, email, password } = accountParams
 
   beforeAll(() => {
     token.validate.mockResolvedValue(id)
+    accountRepository.loadById.mockResolvedValue({ id, email, name, password })
   })
 
   beforeEach(() => {
@@ -40,5 +41,13 @@ describe('AuthorizeUseCase', () => {
 
     expect(accountRepository.loadById).toHaveBeenCalledWith({ id })
     expect(accountRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw PermissionError if LoadAccountById return undefined', async () => {
+    accountRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut({ accessToken })
+
+    await expect(promise).rejects.toThrow(new PermissionError())
   })
 })
