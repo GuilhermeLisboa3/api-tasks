e7defe83f276c67441aef1d8a63fa7afe090bd09
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-09-29 16:50:21 -0300

    feat: ensure JwtAdapter call verify with correct values

--- src/infra/gateways/jwt-adapter.ts
+++ src/infra/gateways/jwt-adapter.ts
@@ -1,4 +1,4 @@
-import { type TokenGenerator } from '@/domain/contracts/gateways'
+import { type TokenGenerator, type TokenValidator } from '@/domain/contracts/gateways'
 
 import * as jwt from 'jsonwebtoken'
 
@@ -7,5 +7,9 @@ export class JwtAdapter implements TokenGenerator {
 
   async generate ({ key }: TokenGenerator.Input): Promise<TokenGenerator.Output> {
     return jwt.sign({ key }, this.secret, { expiresIn: '2d' })
+  }
+
+  async validate ({ token }: TokenValidator.Input): Promise<void> {
+    jwt.verify(token, this.secret)
   }
 }
