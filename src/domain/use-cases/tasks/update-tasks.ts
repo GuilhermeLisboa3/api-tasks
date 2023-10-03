27250a40ba783cd7c154454a328f6a2c7817a619
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-03 13:28:53 -0300

    feat: ensure UpdateTasks throw NotFoundError if LoadTaskById return undefined

--- src/domain/use-cases/tasks/update-tasks.ts
+++ src/domain/use-cases/tasks/update-tasks.ts
@@ -1,4 +1,5 @@
 import { type LoadTaskById } from '@/domain/contracts/database/repositories/tasks'
+import { NotFoundError } from '@/domain/errors'
 
 type Setup = (tasksRepository: LoadTaskById) => UpdateTasks
 type Input = { id: string, title: string, description: string, completed: boolean }
@@ -6,5 +7,6 @@ type Output = void
 export type UpdateTasks = (input: Input) => Promise<Output>
 
 export const updateTasksUseCase: Setup = (tasksRepository) => async ({ description, title, completed, id }) => {
-  await tasksRepository.loadById({ id })
+  const task = await tasksRepository.loadById({ id })
+  if (!task) throw new NotFoundError('id')
 }
