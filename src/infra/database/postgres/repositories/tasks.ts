db5105da363b200db56a72fe89013bcf72d5651f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-03 15:02:08 -0300

    feat: ensure TasksRepository return undefined if task does not exists

--- src/infra/database/postgres/repositories/tasks.ts
+++ src/infra/database/postgres/repositories/tasks.ts
@@ -1,8 +1,13 @@
-import { type AddTasksRepository } from '@/domain/contracts/database/repositories/tasks'
+import { type AddTasksRepository, type LoadTaskById } from '@/domain/contracts/database/repositories/tasks'
 import prisma from '@/infra/database/postgres/helpers/connection'
 
-export class TasksRepository implements AddTasksRepository {
+export class TasksRepository implements AddTasksRepository, LoadTaskById {
   async create ({ accountId, completed, description, title }: AddTasksRepository.Input): Promise<AddTasksRepository.Output> {
     await prisma.task.create({ data: { userId: accountId, completed, description, title } })
+  }
+
+  async loadById ({ id }: LoadTaskById.Input): Promise<LoadTaskById.Output> {
+    const task = await prisma.task.findFirst({ where: { id } })
+    return task ?? undefined
   }
 }
