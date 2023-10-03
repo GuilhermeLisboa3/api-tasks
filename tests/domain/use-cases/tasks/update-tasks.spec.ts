7da7df0add1d4335f2d1a770c75369d0c871c061
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-03 13:29:46 -0300

    test: ensure UpdateTasks throw NotFoundError if LoadTaskById return undefined

--- tests/domain/use-cases/tasks/update-tasks.spec.ts
+++ tests/domain/use-cases/tasks/update-tasks.spec.ts
@@ -1,6 +1,7 @@
 import { tasksParams } from '@/tests/mocks/'
 import { type LoadTaskById } from '@/domain/contracts/database/repositories/tasks'
 import { type UpdateTasks, updateTasksUseCase } from '@/domain/use-cases/tasks'
+import { NotFoundError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -11,6 +12,10 @@ describe('UpdateTasks', () => {
 
   const tasksRepository = mock<LoadTaskById>()
 
+  beforeAll(() => {
+    tasksRepository.loadById.mockResolvedValue({ title, description, id, completed })
+  })
+
   beforeEach(() => {
     sut = updateTasksUseCase(tasksRepository)
   })
@@ -20,5 +25,13 @@ describe('UpdateTasks', () => {
 
     expect(tasksRepository.loadById).toHaveBeenCalledWith({ id })
     expect(tasksRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw NotFoundError if LoadTaskById return undefined', async () => {
+    tasksRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut({ title, description, id, completed })
+
+    await expect(promise).rejects.toThrow(new NotFoundError('id'))
   })
 })
