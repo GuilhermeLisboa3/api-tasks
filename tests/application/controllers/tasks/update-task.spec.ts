eb7dbb3a85c2825d26cb78b4c513efa40220d3a1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-03 15:52:53 -0300

    test: ensure UpdateTaskController return notFound if addTasks return NotFoundError

--- tests/application/controllers/tasks/update-task.spec.ts
+++ tests/application/controllers/tasks/update-task.spec.ts
@@ -1,6 +1,7 @@
 import { tasksParams } from '@/tests/mocks'
 import { UpdateTaskController } from '@/application/controllers/tasks'
 import { Controller } from '@/application/controllers/controller'
+import { NotFoundError } from '@/domain/errors'
 
 describe('UpdateTaskController', () => {
   let sut: UpdateTaskController
@@ -22,5 +23,13 @@ describe('UpdateTaskController', () => {
 
     expect(updateTask).toHaveBeenCalledWith({ id, title, description })
     expect(updateTask).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return notFound if addTasks return NotFoundError', async () => {
+    updateTask.mockRejectedValueOnce(new NotFoundError('id'))
+    const { statusCode, data } = await sut.handle({ id, title, description })
+
+    expect(statusCode).toBe(404)
+    expect(data).toEqual(new NotFoundError('id'))
   })
 })
