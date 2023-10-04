6cf6cbc11d47860b90e532e89856bc64b6149955
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-04 16:10:24 -0300

    test: ensure ListTasksController return ok on success

--- tests/application/controllers/tasks/list-tasks.spec.ts
+++ tests/application/controllers/tasks/list-tasks.spec.ts
@@ -1,4 +1,4 @@
-import { accountParams } from '@/tests/mocks'
+import { accountParams, tasksParams } from '@/tests/mocks'
 import { ListTasksController } from '@/application/controllers/tasks'
 import { Controller } from '@/application/controllers/controller'
 
@@ -8,6 +8,11 @@ describe('ListTasksController', () => {
   const listTasks = jest.fn()
 
   const { id: accountId } = accountParams
+  const { id, title, description, completed } = tasksParams
+
+  beforeAll(() => {
+    listTasks.mockResolvedValue({ id, title, description, completed })
+  })
 
   beforeEach(() => {
     sut = new ListTasksController(listTasks)
@@ -22,5 +27,12 @@ describe('ListTasksController', () => {
 
     expect(listTasks).toHaveBeenCalledWith({ accountId })
     expect(listTasks).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return ok on success', async () => {
+    const { statusCode, data } = await sut.perform({ accountId })
+
+    expect(statusCode).toBe(200)
+    expect(data).toEqual({ id, title, description, completed })
   })
 })
