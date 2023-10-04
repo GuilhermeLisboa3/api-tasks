cdbf320ee0d4468e3bb728511986dcf9522d00b8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-04 16:01:25 -0300

    feat: ensure ListTasksController extend Controller

--- src/application/controllers/tasks/list-tasks.ts
+++ src/application/controllers/tasks/list-tasks.ts
@@ -1,11 +1,14 @@
 import { type ListTasks } from '@/domain/use-cases/tasks'
+import { Controller } from '@/application/controllers/controller'
+import { type HttpResponse } from '@/application/helpers'
 
 type HttpRequest = { accountId: string }
 
-export class ListTasksController {
-  constructor (private readonly listTasks: ListTasks) {}
+export class ListTasksController extends Controller {
+  constructor (private readonly listTasks: ListTasks) { super() }
 
-  async perform (input: HttpRequest): Promise<void> {
+  async perform (input: HttpRequest): Promise<HttpResponse> {
     await this.listTasks(input)
+    return { statusCode: 200, data: null }
   }
 }
