271dbf15e2b70e4ed870b73fdc2f92d67e6fece1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-04 20:15:16 -0300

    test: ensure DeleteTaskUseCase throw NotFoundError if LoadTaskById return undefined

--- tests/domain/use-cases/tasks/delete-task.spec.ts
+++ tests/domain/use-cases/tasks/delete-task.spec.ts
@@ -1,15 +1,20 @@
 import { tasksParams } from '@/tests/mocks/'
 import { type LoadTaskById } from '@/domain/contracts/database/repositories/tasks'
 import { type DeleteTask, deleteTaskUseCase } from '@/domain/use-cases/tasks'
+import { NotFoundError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
 describe('DeleteTask', () => {
   let sut: DeleteTask
 
-  const { id } = tasksParams
+  const { title, description, id, completed } = tasksParams
 
   const tasksRepository = mock<LoadTaskById>()
+
+  beforeAll(() => {
+    tasksRepository.loadById.mockResolvedValue({ title, description, id, completed })
+  })
 
   beforeEach(() => {
     sut = deleteTaskUseCase(tasksRepository)
@@ -20,5 +25,13 @@ describe('DeleteTask', () => {
 
     expect(tasksRepository.loadById).toHaveBeenCalledWith({ id })
     expect(tasksRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw NotFoundError if LoadTaskById return undefined', async () => {
+    tasksRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut({ id })
+
+    await expect(promise).rejects.toThrow(new NotFoundError('id'))
   })
 })
