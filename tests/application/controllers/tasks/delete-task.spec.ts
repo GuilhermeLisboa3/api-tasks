8b7e0f35da6f1dabe2d6f8a6fd366b31ab9c4588
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-04 23:45:32 -0300

    test: ensure DeleteTaskController return notFound if deleteTask return NotFoundError

--- tests/application/controllers/tasks/delete-task.spec.ts
+++ tests/application/controllers/tasks/delete-task.spec.ts
@@ -1,6 +1,7 @@
 import { tasksParams } from '@/tests/mocks'
 import { DeleteTaskController } from '@/application/controllers/tasks'
 import { Controller } from '@/application/controllers/controller'
+import { NotFoundError } from '@/domain/errors'
 
 describe('DeleteTaskController', () => {
   let sut: DeleteTaskController
@@ -22,5 +23,13 @@ describe('DeleteTaskController', () => {
 
     expect(deleteTask).toHaveBeenCalledWith({ id })
     expect(deleteTask).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return notFound if deleteTask return NotFoundError', async () => {
+    deleteTask.mockRejectedValueOnce(new NotFoundError('id'))
+    const { statusCode, data } = await sut.handle({ id })
+
+    expect(statusCode).toBe(404)
+    expect(data).toEqual(new NotFoundError('id'))
   })
 })
